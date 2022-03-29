4d469ad38d368b5108cab4350130548c0e2692ae
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-29 20:48:18 +0500

    Добавил модули и модель

---
+++ main.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const dbConfig = require("./config/db.config");
+const app = express();
+
+// CORS
+var corsOptions = {origin: "http://localhost:8081"};
+app.use(cors(corsOptions));
+
+// BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DB
+const db = require("./models");
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        show_users(); //Show all current users in DB on trmianal
+        
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to application." });
+});
+
+// Routes
+//require("./routes/user.routes")(app,{});
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
+
+
+
+// Add test user admin (dev test)
+function initial(){
+    const User = db.user;
+    const user_test = new User({
+        id: 100,
+        email: "Admin",
+        first_name: "Aydar",
+        last_name: "Mazitov",
+        avatar: "https://reqres.in/img/faces/2-image.jpg"
+    }).save(err => {
+      if(err){
+        console,log("error", err);
+      }
+      console.log("added 'user' to roles cpllection");
+    });
+  }
+
+// Show all users (dev check)
+function show_users() {
+    const User = db.user;
+    User.find({}, function(err, users){
+      if(err) return console.log(err);
+      console.log("All users: ", users);
+    }); 
+  }
---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model(
+    "User", new mongoose.Schema({
+        id:{
+            type: Number,
+            unique: true,
+            required: true,
+        },
+        email:{
+            type: String,
+            unique: true,
+            required: true,
+            minlength:1,
+            maxlength:255
+        },
+        first_name:{
+            type: String,
+            required: false,
+            maxlength:255
+        },
+        lastname:{
+            type: String,
+            required: false,
+            maxlength:255
+        },
+        avatar:{
+            type: String,
+            required: false,
+            maxlength:255
+        }
+    })
+);
+
+module.exports = User;
